c230b86f5b6ab9672d6679f2bd3d95b52b81613b
Samuel Campos <115961998+scampos37@users.noreply.github.com>
2024-04-27 11:23:08 -0700

    Time: 0 ms (100.00%), Space: 41.8 MB (26.77%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,25 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        digits[digits.length-1] = digits[digits.length-1] + 1;
-        for (int i = digits.length-1; i > 0; i--){
-            System.out.println(i);
-            if (digits[i] == 10){
+        for(int i = digits.length-1; i >= 0; i--){
+            if (digits[i] < 9){
+                digits[i] = digits[i] + 1;
+                return digits;
+            }else{
                 digits[i] = 0;
-                digits[i-1] = digits[i-1]+1;
-            }
-            else{
-                break;
             }
         }
-        if (digits[0] == 10){
-            int[] updated = new int[digits.length+1];
-            updated[0] = 1;
-            updated[1] = 0;
-            for(int i = 2; i < updated.length; i++){
-                updated[i] = digits[i-1];
-            }
-            return updated;
-        }
+        digits = new int [digits.length + 1];
+        digits[0] = 1;
         return digits;
     }
 }
